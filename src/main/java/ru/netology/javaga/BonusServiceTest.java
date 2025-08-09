ff18086d56bf56ff450fa0c0e56dd3aaf7579796
Neto test <veronikakaravanova60@gmail.com>
2025-08-09 16:53:55 +0500

    change README

--- src/main/java/ru/netology/javaga/BonusServiceTest.java
+++ src/main/java/ru/netology/javaga/BonusServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.javaga;
 
 import org.testng.annotations.Test;
 import org.junit.jupiter.api.Assertions;
-import ru.netology.javaga.BonusService;
+
 
 public class BonusServiceTest {
 
